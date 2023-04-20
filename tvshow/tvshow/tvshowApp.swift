b5a118321efc4716f9594fb1256382d3941e2919
KY-2187 <kyrahung1@gmail.com>
2023-04-20 15:06:39 +0800

    Upload initial xcode files

---
+++ tvshow/tvshow/tvshowApp.swift
@@ -0,0 +1,17 @@
+//
+//  tvshowApp.swift
+//  tvshow
+//
+//  Created by Kyra Hung on 4/20/23.
+//
+
+import SwiftUI
+
+@main
+struct tvshowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
